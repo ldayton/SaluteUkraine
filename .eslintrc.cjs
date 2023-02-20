e9019f6b44335dc1e056b1b836bca72ba70cdf61
Lily Dayton <lily.dayton.git@gmail.com>
2023-02-20 15:40:56 -0500

    set up prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+};
