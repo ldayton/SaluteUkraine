8542f311c03d39be3d0c12c8c71e0f0bd26876c2
Lily Dayton <lily.dayton.git@gmail.com>
2023-02-20 10:53:34 -0500

    add Tailwind and Material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
+          <p className="underline">
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
