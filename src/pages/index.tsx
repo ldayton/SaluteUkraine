32326ac071f97bf69505208ea48e900be5bdd8ec
Lily Dayton <lily.dayton.git@gmail.com>
2023-03-04 14:06:10 -0500

    add twitter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Flag() {
         <meta name="twitter:card" content="summary" />
       </Head>
       <main className="flex flex-col items-center bg-white">
-        <header className="header flex flex-row p-4 pb-3 items-center text-center shadow">
+        <header className="header flex flex-row p-4 pb-3 items-center text-center">
           <h1 className="headline text-4xl font-bold">Salute Ukraine!</h1>
         </header>
         <section className="hero w-full shadow">
@@ -43,27 +43,32 @@ export default function Flag() {
           <div className="icons bg-white flex flex-row flex-wrap justify-center p-4 gap-8 text-black underline">
             <Icon
               name="Telegram"
-              path="icons/telegram_syolpz.svg"
+              path="icons/telegram_syolpz.png"
               href="https://t.me/SaluteUkraine"
             />
             <Icon
               name="YouTube"
-              path="icons/youtube_hvxem0.svg"
+              path="icons/youtube_hvxem0.png"
               href="https://www.youtube.com/channel/UCLNXzp2NKjrEfmWjTJJykcA"
             />
             <Icon
+              name="Twitter"
+              path="icons/twitter_gcfeyc.png"
+              href="https://twitter.com/SaluteUkraine"
+            />
+            <Icon
               name="LinkedIn"
-              path="icons/linkedin_togz5a.svg"
+              path="icons/linkedin_togz5a.png"
               href="https://www.linkedin.com/in/lily-dayton/"
             />
             <Icon
               name="Facebook"
-              path="icons/facebook_s2xkqt.svg"
+              path="icons/facebook_s2xkqt.png"
               href="https://www.facebook.com/profile.php?id=100090066385190"
             />
             <Icon
               name="E-Mail"
-              path="icons/email_vwkeyr.svg"
+              path="icons/email_vwkeyr.png"
               href="mailto:lily.dayton.3@gmail.com"
             />
           </div>
@@ -113,15 +118,23 @@ type IconProps = {
 };
 
 function Icon({ href, name, path }: IconProps) {
-  const size = 40;
+  const imageSize = 40;
+  const containerSize = 90;
   return (
-    <div className="flex flex-col items-center text-center" style={{ width: 70, height: 70 }}>
-      <a className="flex flex-col items-center w-full h-full justify-between" href={href} target="_blank">
+    <div
+      className="flex flex-col items-center text-center"
+      style={{ width: containerSize, height: containerSize }}
+    >
+      <a
+        className="flex flex-col items-center w-full h-full justify-between p-4 border border-black rounded-lg"
+        href={href}
+        target="_blank"
+      >
         <img
-          src={`https://res.cloudinary.com/dxjzrhogi/image/upload/w_${size},h_${size},c_fill/v1677879795/salute-ukraine/${path}`}
+          src={`https://res.cloudinary.com/dxjzrhogi/image/upload/w_${imageSize},h_${imageSize},ar_1/v1677879795/salute-ukraine/${path}`}
           alt={`${name} Logo`}
-          width={size}
-          height={size}
+          width={imageSize}
+          height={imageSize}
         />
         <div>{`${name}`}</div>
       </a>
