78b82b66a8d8bbdbce098b10c9ca7210c329e070
Lily Dayton <lily.dayton.git@gmail.com>
2023-03-05 06:18:06 -0500

    use inline svg social icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import { JsxChild, JsxElement } from "typescript";
 
 const previewUrl = "https://www.saluteukraine.com/";
 const previewThumbnail =
@@ -97,7 +98,7 @@ export default function Flag() {
             />
           </div>
         </div>
-        <div className="contact text-xl p-4 shadow-xl rounded  w-full flex flex-col gap-y-2">
+        <div className="contact text-xl p-4 pb-12 shadow-xl rounded  w-full flex flex-col gap-y-2">
           <div className="text-center">
             Contact{" "}
             <a className="underline" href="mailto:lily.dayton.3@gmail.com">
@@ -117,27 +118,44 @@ type IconProps = {
   path: string;
 };
 
-function Icon({ href, name, path }: IconProps) {
-  const imageSize = 40;
-  const containerSize = 90;
+function Icon({ href, name }: IconProps) {
   return (
-    <div
-      className="flex flex-col items-center text-center"
-      style={{ width: containerSize, height: containerSize }}
-    >
-      <a
-        className="flex flex-col items-center w-full h-full justify-between p-4 border border-black rounded-lg"
-        href={href}
-        target="_blank"
+    <a href={href}>
+      <button
+        type="button"
+        className="text-white bg-[#2b58b8] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm text-center flex flex-col items-center justify-between dark:focus:ring-[#3b5998]/55 mr-2 mb-2 w-28 h-28 p-3 pt-6"
       >
-        <img
-          src={`https://res.cloudinary.com/dxjzrhogi/image/upload/w_${imageSize},h_${imageSize},ar_1/v1677879795/salute-ukraine/${path}`}
-          alt={`${name} Logo`}
-          width={imageSize}
-          height={imageSize}
-        />
-        <div>{`${name}`}</div>
-      </a>
-    </div>
+        <svg
+          className="w-10 h-10 mr-2 -ml-1"
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512"
+        >
+          <path fill="currentColor" d={iconPaths[name]}></path>
+        </svg>
+        {name}
+      </button>
+    </a>
   );
 }
+
+type IconMap = {
+  [name: string]: string;
+};
+
+const iconPaths: IconMap = {
+  email:
+    "M101.3 141.6v228.9h0.3 308.4 0.8V141.6H101.3zM375.7 167.8l-119.7 91.5 -119.6-91.5H375.7zM127.6 194.1l64.1 49.1 -64.1 64.1V194.1zM127.8 344.2l84.9-84.9 43.2 33.1 43-32.9 84.7 84.7L127.8 344.2 127.8 344.2zM384.4 307.8l-64.4-64.4 64.4-49.3V307.8z",
+  facebook:
+    "M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z",
+  twitter:
+    "M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z",
+  linkedin:
+    "M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z",
+  youtube:
+    "M422.6 193.6c-5.3-45.3-23.3-51.6-59-54 -50.8-3.5-164.3-3.5-215.1 0 -35.7 2.4-53.7 8.7-59 54 -4 33.6-4 91.1 0 124.8 5.3 45.3 23.3 51.6 59 54 50.9 3.5 164.3 3.5 215.1 0 35.7-2.4 53.7-8.7 59-54C426.6 284.8 426.6 227.3 422.6 193.6zM222.2 303.4v-94.6l90.7 47.3L222.2 303.4z",
+  telegram:
+    "M417.28 795.733333 429.226667 615.253333 756.906667 320C771.413333 306.773333 753.92 300.373333 734.72 311.893333L330.24 567.466667 155.306667 512C117.76 501.333333 117.333333 475.306667 163.84 456.533333L845.226667 193.706667C876.373333 179.626667 906.24 201.386667 894.293333 249.173333L778.24 795.733333C770.133333 834.56 746.666667 843.946667 714.24 826.026667L537.6 695.466667 452.693333 777.813333C442.88 787.626667 434.773333 795.733333 417.28 795.733333Z",
+};
