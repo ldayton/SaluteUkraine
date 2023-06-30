d4f1ccfb20880726675f8baccb757a00c4276eb9
Lily Dayton <43729618+ldayton@users.noreply.github.com>
2023-06-30 08:44:16 -0400

    Remove Reddit and LinkedIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,14 +52,14 @@ export default function Flag() {
             </div>
           </div>
           <div className="icons bg-white flex flex-row flex-wrap justify-center pl-2 pt-4 pb-2 gap-2 text-black underline">
-            <Icon name="YouTube" href="https://www.youtube.com/@SaluteUkraine" />
             <Icon name="Instagram" href="https://www.instagram.com/saluteukraine/" />
+            <Icon name="YouTube" href="https://www.youtube.com/@SaluteUkraine" />
             <Icon name="Facebook" href="https://www.facebook.com/SaluteUkraineNYC" />
             <Icon name="TikTok" href="https://www.tiktok.com/@saluteukraine" />
             <Icon name="Twitter" href="https://twitter.com/SaluteUkraine" />
             <Icon name="Telegram" href="https://t.me/SaluteUkraine" />
-            <Icon name="LinkedIn" href="https://www.linkedin.com/company/saluteukraine/" />
-            <Icon name="Reddit" href="https://www.reddit.com/r/SaluteUkraine/" />
+{/*             <Icon name="LinkedIn" href="https://www.linkedin.com/company/saluteukraine/" /> */}
+{/*             <Icon name="Reddit" href="https://www.reddit.com/r/SaluteUkraine/" /> */}
           </div>
         </section>
         <div className="grid grid-cols-1 p-8 pt-4">
